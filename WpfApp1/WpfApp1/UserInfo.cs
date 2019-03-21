8e8b74391a959b0eec83fe68b236f1c67e1ff617
Vertigo <yosso@mail.ru>
2019-03-21 13:32:22 +0300

    d

--- WpfApp1/WpfApp1/UserInfo.cs
+++ WpfApp1/WpfApp1/UserInfo.cs
@@ -42,7 +42,7 @@ namespace WpfApp1
             counter = Convert.ToInt32(outputParameter.Value);
             con.Close();
 
-            if (counter > 1)
+            if (counter > 0)
                 return true;
             else
                 return false;
